7c11202e156f56a05d5115d36b71badcbbaef117
zerosand1s <harshal.gangurde@infostretch.com>
2020-03-15 12:33:59 +0530

    add login view component

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <header>
+      <h2>Login to explore blogs</h2>
+    </header>
+    <form action="#" method="post">
+      <div class="input">
+        <input type="text" name="username" />
+      </div>
+      <div class="input">
+        <input type="text" name="password" />
+      </div>
+      <div class="input">
+        <button type="submit">Login</button>
+      </div>
+    </form>
+    <footer>
+      <span>Do not have an account yet?</span>
+      &nbsp;&nbsp;&nbsp;
+      <a href="#">Register here</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import url("Login.scss");
+</style>
