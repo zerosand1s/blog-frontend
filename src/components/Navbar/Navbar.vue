b2907751e994a6f00b92b97d2ba95e465f95236f
zerosand1s <harshal.gangurde@infostretch.com>
2020-04-04 23:44:22 +0530

    add navbar component

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="navbar">
+      <ul class="nav-list">
+        <li :class="[currentPath.includes('home') ? activeClass : '', 'nav-item']">
+          <router-link to="/home">Home</router-link>
+        </li>
+        <li :class="[currentPath.includes('profile') ? activeClass : '', 'nav-item']">
+          <router-link to="/profile">Profile</router-link>
+        </li>
+        <li :class="[currentPath.includes('signout') ? activeClass : '', 'nav-item']">
+          <router-link to="/signout">Signout</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      activeClass: 'active'
+    };
+  },
+  computed: {
+    currentPath() {
+      return this.$route.path;
+    }
+  }
+};
+</script> 
+
+<style lang="scss" scoped>
+@import url('Navbar.scss');
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Home</h1>
+  <div class="container">
+    <Navbar></Navbar>
+    <h1>Home</h1>
+  </div>
 </template>
+
+<script>
+import Navbar from '../../components/Navbar/Navbar';
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar
+  }
+};
+</script>
