965326505628c02512dafe56619841d12ba19c00
zerosand1s <harshal.gangurde@infostretch.com>
2020-04-08 11:43:44 +0530

    add error message component

---
+++ src/components/ErrorMessage/ErrorMessage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="error-message-container">
+    <div class="error-message">{{ message }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error-message',
+  props: ['message']
+};
+</script>
+
+<style lang="scss" scoped>
+.error-message-container {
+  width: 22%;
+  height: 8%;
+  display: flex;
+  // align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 2%;
+  right: 2%;
+  color: #000000;
+  border-radius: 8px;
+  background-color: #ffcccb;
+  box-shadow: 2px 2px 8px 5px #ffcccb;
+  z-index: 1000;
+}
+
+.error-message {
+  display: flex;
+  margin: auto;
+  text-align: center;
+}
+</style>
