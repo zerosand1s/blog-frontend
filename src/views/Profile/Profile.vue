df8026263bbb961a9c628ec17a444d05e929005a
Harshal <gangurdeharsh08@gmail.com>
2020-04-25 12:35:46 +0530

    refactor profile component

--- src/views/Profile/Profile.vue
+++ src/views/Profile/Profile.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="body">
-    <error-message v-if="showError" v-bind:message="errorMessage"></error-message>
+  <div class="container">
     <navbar></navbar>
-    <div class="profile-container">
+    <div class="profile">
+      <error-message v-if="showError" v-bind:message="errorMessage"></error-message>
       <div class="user-details">
         <h1>{{ user.firstName }} {{ user.lastName }}</h1>
         <h4>@{{ user.username }}</h4>
         <div>{{ user.email }}</div>
       </div>
-      <div class="user-blog-list">
-        <blog-list v-bind:blogs="user.blogs"></blog-list>
+      <div class="user-blogs">
+        <aside class="sidebar">
+          <topics v-bind:tags="user.tags"></topics>
+        </aside>
+        <main class="blog-list">
+          <blog-list v-bind:blogs="user.blogs"></blog-list>
+        </main>
       </div>
     </div>
   </div>
@@ -19,12 +24,14 @@
 const userService = require('../../services/user.service');
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
 import Navbar from '../../components/Navbar/Navbar';
+import Topics from '../../components/Topics';
 import BlogList from '../../components/BlogList/BlogList';
 export default {
   components: {
     Navbar,
     ErrorMessage,
-    BlogList
+    BlogList,
+    Topics
   },
   data() {
     return {
@@ -42,9 +49,10 @@ export default {
       try {
         const token = localStorage.getItem('token');
         const username = localStorage.getItem('username');
+        const tags = ['travel', 'sports', 'coding'];
         const user = await userService.getUserDetails(token, username);
+        user.tags = tags;
         this.user = user;
-        console.log(this.user);
         this.loading = false;
       } catch (err) {
         this.errorMessage = err.response.data.message;
@@ -60,5 +68,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import url('Profile.scss');
+.profile {
+  display: grid;
+  grid-template-columns: 25% auto;
+  grid-template-rows: auto;
+  grid-gap: 1rem;
+  grid-template-areas: 'user-details user-details' 'user-blogs user-blogs';
+  margin: 1rem 5rem;
+}
+
+.user-details {
+  width: auto;
+  // margin: 1rem 5rem;
+  display: flex;
+  flex-direction: column;
+  padding: 0.8rem;
+  text-align: center;
+  grid-area: user-details;
+  border: 0.5px solid $primary;
+  border-radius: $default-border-radius;
+}
+
+.user-blogs {
+  width: 100%;
+  display: grid;
+  grid-area: user-blogs;
+  grid-template-columns: 25% auto;
+  grid-template-rows: auto;
+  grid-template-areas: 'sidebar blog-list';
+  grid-gap: 0 1rem;
+}
+
+.sidebar {
+  width: 100%;
+  grid-area: sidebar;
+}
+
+.blog-list {
+  width: 100%;
+  grid-area: blog-list;
+}
 </style>
